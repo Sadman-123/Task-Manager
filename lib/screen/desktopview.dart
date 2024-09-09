72644dfa487e4a676cb3b38e42f11a35904dccaa
Sadman-123 <19201103123@cse.bubt.edu.bd>
2024-09-10 03:45:22 +0600

    just made welcome screen for desktop

--- lib/screen/desktopview.dart
+++ lib/screen/desktopview.dart
@@ -2,10 +2,43 @@ import 'package:flutter/material.dart';
 class Desktopview extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var mdw=MediaQuery.of(context).size.width;
+    var mdh=MediaQuery.of(context).size.height;
     return Scaffold(
-      body: Center(
-        child: Text("Desktop"),
-      ),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(height: mdh*0.18,),
+                Text("Task Manager",style: TextStyle(fontSize: mdw*0.053,fontWeight: FontWeight.bold),),
+                Text("Stay on Track, Stay Productive",style: TextStyle(fontSize: mdw*0.02,color: Colors.black54),),
+                SizedBox(height: mdh*0.09,),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Get Started!",
+                    style: TextStyle(fontSize: mdh*0.03,color: Colors.white),
+                  ),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.blueAccent.shade400),
+                    padding: MaterialStateProperty.all<EdgeInsets>(EdgeInsets.symmetric(horizontal: 40, vertical: 25)), // Adjust padding
+                    minimumSize: MaterialStateProperty.all(Size(150, 50)), // Set minimum button size
+                  ),
+                ),
+              ],
+            ),
+          ),
+         Container(
+           child: Image.asset("assets/task.png",
+           height: mdh*0.7,
+             width: mdw*0.5,
+           ),
+         )
+        ],
+      )
     );
   }
 }
